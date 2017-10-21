1d40c5c31033c0f4b671eb9f24d92d27bb9d982c
kevin <kevin.robbins14@gmail.com>
2017-10-21 06:36:01 -0400

    test

--- MongoClient.py
+++ MongoClient.py
@@ -10,11 +10,13 @@ def GetFish():
     fish_table = collection.find({"name":"Reef Shark"})
     fishstr = None
     for document in fish_table:
+        print 'hi'
         print document
         #fishstr = pprint.pformat(document)
     with open('fish.json', 'w') as file:
         json.dump(fishstr, file)
     file.close()
+    client.close()
 
     #we can store images on our server and just send the link from the db
     #arr = {'name', 'description', 'image'}
