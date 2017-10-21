50474d4d8e847f261a95de9f3a3dfa6ae69991ae
kevin <kevin.robbins14@gmail.com>
2017-10-20 21:57:42 -0400

    update on mongodb

--- MongoClient.py
+++ MongoClient.py
@@ -1,3 +1,14 @@
 import pymongo
 from pymongo import MongoClient
 
+def GetFish():
+    client = MongoClient('172.31.50.92', 8080)
+    collection = db.fish
+    
+
+def GetTrash():
+    return
+
+def GetNotTrash():
+    return
+
