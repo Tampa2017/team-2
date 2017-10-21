14b79737449bd5d5f1131b17fe90e0e22ac581d6
kevin <kevin.robbins14@gmail.com>
2017-10-20 23:24:15 -0400

    Changing database return
    Changed so that we can send json files instead of arrays of strings

--- MongoClient.py
+++ MongoClient.py
@@ -5,9 +5,14 @@ def GetFish():
     client = MongoClient('172.31.50.92', 27017)
     collection = db.fish
     fish_table = collection.find_one({"fish" : "fish1"})
+    file = open('fish.json', 'w')
+    file.write(fish_table)
+    file.close()
+
     #we can store images on our server and just send the link from the db
-    arr = {'name', 'description', 'image'}
-    toPost = fish_table[arr]
+    #arr = {'name', 'description', 'image'}
+    #toPost = fish_table[arr]
+    #print statement for testing
     #Post
 
 
@@ -15,17 +20,27 @@ def GetTrash():
     client = MongoClient('172.31.50.92', 8080)
     collection = db.items.negative
     trash_table = collection.find_one({"trash" : "bottle"})
+    file = open('trash.json', 'w')
+    file.write(trash_table)
+    file.close()
+
     #we can store images on our server and just send the link from the db
-    arr = {'name', 'description', 'image'}
+    #arr = {'name', 'description', 'image'}
     toPost = trash_table[arr]
+    # print statement for testing
     #Post
 
 def GetNotTrash():
     client = MongoClient('172.31.50.92', 8080)
     collection = db.items.positive
-    trash_table = collection.find_one({"trash": "reef"})
+    good_table = collection.find_one({"good": "reef"})
+    file = open('good.json', 'w')
+    file.write(good_tables)
+    file.close()
+
     # we can store images on our server and just send the link from the db
-    arr = {'name', 'description', 'image'}
+    #arr = {'name', 'description', 'image'}
     toPost = trash_table[arr]
+    # print statement for testing
     # Post
 
