b9f5932754210bbd9549e789b03967219a252594
kevin <kevin.robbins14@gmail.com>
2017-10-20 22:50:25 -0400

    Mongo client complete
    Still needs testing but db info should be able to be retreived.

--- MongoClient.py
+++ MongoClient.py
@@ -2,13 +2,30 @@ import pymongo
 from pymongo import MongoClient
 
 def GetFish():
-    client = MongoClient('172.31.50.92', 8080)
+    client = MongoClient('172.31.50.92', 27017)
     collection = db.fish
-    
+    fish_table = collection.find_one({"fish" : "fish1"})
+    #we can store images on our server and just send the link from the db
+    arr = {'name', 'description', 'image'}
+    toPost = fish_table[arr]
+    #Post
+
 
 def GetTrash():
-    return
+    client = MongoClient('172.31.50.92', 8080)
+    collection = db.items.negative
+    trash_table = collection.find_one({"trash" : "bottle"})
+    #we can store images on our server and just send the link from the db
+    arr = {'name', 'description', 'image'}
+    toPost = trash_table[arr]
+    #Post
 
 def GetNotTrash():
-    return
+    client = MongoClient('172.31.50.92', 8080)
+    collection = db.items.positive
+    trash_table = collection.find_one({"trash": "reef"})
+    # we can store images on our server and just send the link from the db
+    arr = {'name', 'description', 'image'}
+    toPost = trash_table[arr]
+    # Post
 
